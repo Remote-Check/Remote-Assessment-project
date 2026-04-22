71ff7c5360a38d339a9c5645d3afa27f3b499d64
Etay Cohen <etaycohen@Etays-Air.lan>
2026-04-22 08:30:15 +0300

    test: add @playwright/test dev dependency and configure vitest to exclude e2e tests

--- client/vitest.config.ts
+++ client/vitest.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vitest/config';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
   test: {
-    environment: 'jsdom',
-    globals: true,
-    setupFiles: './src/test/setup.ts',
+    exclude: ['**/node_modules/**', '**/dist/**', '**/cypress/**', '**/.{idea,git,cache,output,temp}/**', '**/{karma,rollup,webpack,vite,vitest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*', 'tests/e2e/**'],
   },
 });
